fa37739c154780670cdd26952503e85fd3ec35f5
Barış Çetintürk <baris.cetinturk.ytu@gmail.com>
2020-10-13 12:30:23 +0300

    Added resilience4j config to gateway with fallback

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Slf4j
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -16,10 +20,10 @@ public class ApiGatewayApplication {
 	}
 
 	@Bean
-	RouteLocator routes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("user-service", p -> p.path("/users", "/users/*").uri("lb://user-service"))
-				.build();
+	public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
+		ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
+		reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+		return reactiveResilience4JCircuitBreakerFactory;
 	}
 
 }
---
+++ api-gateway/src/main/java/com/example/apigateway/controller/FallbackController.java
@@ -0,0 +1,13 @@
+package com.example.apigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public String fallback() {
+        return "Service currently unavailable";
+    }
+}
